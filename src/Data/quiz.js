527e141c5cd0f9ffb4f049ff3c7c6c23f00957c2
Bendik Skarpnes <bendik.skarpnes@gcegal.com>
2022-01-12 13:01:20 +0100

    update card definition

--- src/Data/quiz.js
+++ src/Data/quiz.js
@@ -1,356 +1,315 @@
+// {
+//   id: incrementally increase this number,
+//   tool_name: 'name of tool',
+//   icon: 'name of image file with the of tool', 
+//   images: ['adobeillustrator.png', 'adobeillustrator.png', 'adobeillustrator.png'],
+//   text: 'tool explainer text',
+//   video_link: 'make sure to use youtube.com/embed or player.vimeo.com'
+// }
+
+// every picture should be saved at public/assets/img/
+
+
 export default [
   {
     id: 0,
-    quiz_name: 'adobeillustrator',
-    file: 'adobe-illustrator.json',
-    img: 'adobeillustrator.png'
+    tool_name: 'toolbox test',
+    icon: 'adobeillustrator.png',
+    images: ['adobeillustrator.png', 'adobeillustrator.png', 'adobeillustrator.png'],
+    text: 'Make existing Petrel workflows more efficient with the Blueback Unconventional Toolbox. With tools like The Well Data Viewer you can achieve streamlined and efficient well data browsing. The Log Normalization tool, the Global Well Log manager and the Rename Tools provide basic data preparation that is essential in projects with many wells. Geological and petrophysical trend screening can be performed using the Quick Mapper and the Automated tops gridder.',
+    video_link: 'https://www.youtube.com/embed/a9PadVgYzvE'
   },
   {
     id: 1,
-    quiz_name: 'microsoftexcel',
-    file: 'microsoft-excel.json',
-    img: 'microsoftexcel.png'
+    tool_name: 'example name',
+    icon: 'python.png',
+    images: ['python.png'],
+    text: 'This text explains how the tool works!',
+    video_link: 'https://www.youtube.com/embed/a9PadVgYzvE'
   },
   {
     id: 2,
-    quiz_name: 'adobephotoshop',
-    file: 'adobe-photoshop.json',
-    img: 'adobephotoshop.png'
+    tool_name: 'adobephotoshop',
+    video_link: '', text: '', images: [], icon: 'adobephotoshop.png'
   },
   {
     id: 3,
-    quiz_name: 'microsoftoutlook',
-    file: 'microsoft-outlook.json',
-    img: 'microsoftoutlook.png'
+    tool_name: 'microsoftoutlook',
+    video_link: '', text: '', images: [], icon: 'microsoftoutlook.png'
   },
   {
     id: 4,
-    quiz_name: 'agilemethodologies',
-    file: 'agile-methodologies.json',
-    img: 'agilemethodologies.png'
+    tool_name: 'agilemethodologies',
+    video_link: '', text: '', images: [], icon: 'agilemethodologies.png'
   },
   {
     id: 5,
-    quiz_name: 'microsoftpower-bi',
-    file: 'microsoft-power-bi.json',
-    img: 'microsoftpower-bi.png'
+    tool_name: 'microsoftpower-bi',
+    video_link: '', text: '', images: [], icon: 'microsoftpower-bi.png'
   },
   {
     id: 6,
-    quiz_name: 'android',
-    file: 'android.json',
-    img: 'android.png'
+    tool_name: 'android',
+    video_link: '', text: '', images: [], icon: 'android.png'
   },
   {
     id: 7,
-    quiz_name: 'microsoftpower-point',
-    file: 'microsoft-power-point.json',
-    img: 'microsoftpower-point.png'
+    tool_name: 'microsoftpower-point',
+    video_link: '', text: '', images: [], icon: 'microsoftpower-point.png'
   },
   {
     id: 8,
-    quiz_name: 'angularjs',
-    file: 'angularjs.json',
-    img: 'angularjs.png'
+    tool_name: 'angularjs',
+    video_link: '', text: '', images: [], icon: 'angularjs.png'
   },
   {
     id: 9,
-    quiz_name: 'microsoftproject',
-    file: 'microsoft-project.json',
-    img: 'microsoftproject.png'
+    tool_name: 'microsoftproject',
+    video_link: '', text: '', images: [], icon: 'microsoftproject.png'
   },
   {
     id: 10,
-    quiz_name: 'aws',
-    file: 'aws.json',
-    img: 'aws.png'
+    tool_name: 'aws',
+    video_link: '', text: '', images: [], icon: 'aws.png'
   },
   {
     id: 11,
-    quiz_name: 'microsoftword',
-    file: 'microsoft-word.json',
-    img: 'microsoftword.png'
+    tool_name: 'microsoftword',
+    video_link: '', text: '', images: [], icon: 'microsoftword.png'
   },
   {
     id: 12,
-    quiz_name: 'awslambda',
-    file: 'aws-lambda.json',
-    img: 'awslambda.png'
+    tool_name: 'awslambda',
+    video_link: '', text: '', images: [], icon: 'awslambda.png'
   },
   {
     id: 13,
-    quiz_name: 'mongodb',
-    file: 'mongodb.json',
-    img: 'mongodb.png'
+    tool_name: 'mongodb',
+    video_link: '', text: '', images: [], icon: 'mongodb.png'
   },
   {
     id: 14,
-    quiz_name: 'bash',
-    file: 'bash.json',
-    img: 'bash.jpg'
+    tool_name: 'bash',
+    video_link: '', text: '', images: [], icon: 'bash.jpg'
   },
   {
     id: 15,
-    quiz_name: 'mysql',
-    file: 'mysql.json',
-    img: 'mysql.png'
+    tool_name: 'mysql',
+    video_link: '', text: '', images: [], icon: 'mysql.png'
   },
   {
     id: 16,
-    quiz_name: 'c',
-    file: 'c.json',
-    img: 'c.png'
+    tool_name: 'c',
+    video_link: '', text: '', images: [], icon: 'c.png'
   },
   {
     id: 17,
-    quiz_name: 'node',
-    file: 'node.js.json',
-    img: 'node.js.png'
+    tool_name: 'node',
+    video_link: '', text: '', images: [], icon: 'node.js.png'
   },
   {
     id: 18,
-    quiz_name: 'c++quiz',
-    file: 'c++quiz.md.json',
-    img: 'c++quiz.png'
+    tool_name: 'c++quiz',
+    video_link: '', text: '', images: [], icon: 'c++quiz.png'
   },
   {
     id: 19,
-    quiz_name: 'nosql',
-    file: 'nosql.json',
-    img: 'nosql.png'
+    tool_name: 'nosql',
+    video_link: '', text: '', images: [], icon: 'nosql.png'
   },
   {
     id: 20,
-    quiz_name: 'csharp',
-    file: 'c-sharp.json',
-    img: 'csharp.png'
+    tool_name: 'csharp',
+    video_link: '', text: '', images: [], icon: 'csharp.png'
   },
   {
     id: 21,
-    quiz_name: 'objectivec',
-    file: 'objective-c.json',
-    img: 'objectivec.png'
+    tool_name: 'objectivec',
+    video_link: '', text: '', images: [], icon: 'objectivec.png'
   },
   {
     id: 22,
-    quiz_name: 'css',
-    file: 'css.json',
-    img: 'css.png'
+    tool_name: 'css',
+    video_link: '', text: '', images: [], icon: 'css.png'
   },
   {
     id: 23,
-    quiz_name: 'objectoriented-programming',
-    file: 'object-oriented-programming.json',
-    img: 'objectoriented-programming.png'
+    tool_name: 'objectoriented-programming',
+    video_link: '', text: '', images: [], icon: 'objectoriented-programming.png'
   },
   {
     id: 24,
-    quiz_name: 'django',
-    file: 'django.json',
-    img: 'django.png'
+    tool_name: 'django',
+    video_link: '', text: '', images: [], icon: 'django.png'
   },
   {
     id: 25,
-    quiz_name: 'php',
-    file: 'php.json',
-    img: 'php.png'
+    tool_name: 'php',
+    video_link: '', text: '', images: [], icon: 'php.png'
   },
   {
     id: 26,
-    quiz_name: 'dotnetframework',
-    file: 'dotnet-framework.json',
-    img: 'dotnetframework.png'
+    tool_name: 'dotnetframework',
+    video_link: '', text: '', images: [], icon: 'dotnetframework.png'
   },
   {
     id: 27,
-    quiz_name: 'python',
-    file: 'python.json',
-    img: 'python.png'
+    tool_name: 'python',
+    video_link: '', text: '', images: [], icon: 'python.png'
   },
   {
     id: 28,
-    quiz_name: 'gcp',
-    file: 'gcp.json',
-    img: 'gcp.png'
+    tool_name: 'gcp',
+    video_link: '', text: '', images: [], icon: 'gcp.png'
   },
   {
     id: 29,
-    quiz_name: 'quickbooks',
-    file: 'quickbooks.json',
-    img: 'quickbooks.svg'
+    tool_name: 'quickbooks',
+    video_link: '', text: '', images: [], icon: 'quickbooks.svg'
   },
   {
     id: 30,
-    quiz_name: 'git',
-    file: 'git.json',
-    img: 'git.png'
+    tool_name: 'git',
+    video_link: '', text: '', images: [], icon: 'git.png'
   },
   {
     id: 31,
-    quiz_name: 'reactjs',
-    file: 'reactjs.json',
-    img: 'reactjs.png'
+    tool_name: 'reactjs',
+    video_link: '', text: '', images: [], icon: 'reactjs.png'
   },
   {
     id: 32,
-    quiz_name: 'googleanalytics',
-    file: 'google-analytics.json',
-    img: 'googleanalytics.png'
+    tool_name: 'googleanalytics',
+    video_link: '', text: '', images: [], icon: 'googleanalytics.png'
   },
   {
     id: 33,
-    quiz_name: 'restapi',
-    file: 'rest-api.json',
-    img: 'restapi.png'
+    tool_name: 'restapi',
+    video_link: '', text: '', images: [], icon: 'restapi.png'
   },
   {
     id: 34,
-    quiz_name: 'hadoop',
-    file: 'hadoop.json',
-    img: 'hadoop.png'
+    tool_name: 'hadoop',
+    video_link: '', text: '', images: [], icon: 'hadoop.png'
   },
   {
     id: 35,
-    quiz_name: 'r',
-    file: 'r.json',
-    img: 'r.png'
+    tool_name: 'r',
+    video_link: '', text: '', images: [], icon: 'r.png'
   },
   {
     id: 36,
-    quiz_name: 'html',
-    file: 'html.json',
-    img: 'html.png'
+    tool_name: 'html',
+    video_link: '', text: '', images: [], icon: 'html.png'
   },
   {
     id: 37,
-    quiz_name: 'rubyon-rails',
-    file: 'ruby-on-rails.json',
-    img: 'rubyon-rails.png'
+    tool_name: 'rubyon-rails',
+    video_link: '', text: '', images: [], icon: 'rubyon-rails.png'
   },
   {
     id: 38,
-    quiz_name: 'java',
-    file: 'java.json',
-    img: 'java.png'
+    tool_name: 'java',
+    video_link: '', text: '', images: [], icon: 'java.png'
   },
   {
     id: 39,
-    quiz_name: 'scala',
-    file: 'scala.json',
-    img: 'scala.png'
+    tool_name: 'scala',
+    video_link: '', text: '', images: [], icon: 'scala.png'
   },
   {
     id: 40,
-    quiz_name: 'javascript',
-    file: 'javascript.json',
-    img: 'javascript.png'
+    tool_name: 'javascript',
+    video_link: '', text: '', images: [], icon: 'javascript.png'
   },
   {
     id: 41,
-    quiz_name: 'searchengine-optimization',
-    file: 'search-engine-optimization.json',
-    img: 'searchengine-optimization.jpg'
+    tool_name: 'searchengine-optimization',
+    video_link: '', text: '', images: [], icon: 'searchengine-optimization.jpg'
   },
   {
     id: 42,
-    quiz_name: 'jquery',
-    file: 'jquery.json',
-    img: 'jquery.jpg'
+    tool_name: 'jquery',
+    video_link: '', text: '', images: [], icon: 'jquery.jpg'
   },
   {
     id: 43,
-    quiz_name: 'sharepoint',
-    file: 'sharepoint.json',
-    img: 'sharepoint.png'
+    tool_name: 'sharepoint',
+    video_link: '', text: '', images: [], icon: 'sharepoint.png'
   },
   {
     id: 44,
-    quiz_name: 'json',
-    file: 'json.json',
-    img: 'json.png'
+    tool_name: 'json',
+    video_link: '', text: '', images: [], icon: 'json.png'
   },
   {
     id: 45,
-    quiz_name: 'swift',
-    file: 'swift.json',
-    img: 'swift.png'
+    tool_name: 'swift',
+    video_link: '', text: '', images: [], icon: 'swift.png'
   },
   {
     id: 46,
-    quiz_name: 'kotlin',
-    file: 'kotlin.json',
-    img: 'kotlin.jpeg'
+    tool_name: 'kotlin',
+    video_link: '', text: '', images: [], icon: 'kotlin.jpeg'
   },
   {
     id: 47,
-    quiz_name: 'tsql',
-    file: 't-sql.json',
-    img: 'tsql.png'
+    tool_name: 'tsql',
+    video_link: '', text: '', images: [], icon: 'tsql.png'
   },
   {
     id: 48,
-    quiz_name: 'linuxassesment',
-    file: 'linux-assesment.md.json',
-    img: 'linuxassesment.png'
+    tool_name: 'linuxassesment',
+    video_link: '', text: '', images: [], icon: 'linuxassesment.png'
   },
   {
     id: 49,
-    quiz_name: 'vba',
-    file: 'vba.json',
-    img: 'vba.webp'
+    tool_name: 'vba',
+    video_link: '', text: '', images: [], icon: 'vba.webp'
   },
   {
     id: 50,
-    quiz_name: 'machinelearning',
-    file: 'machine-learning.json',
-    img: 'machinelearning.png'
+    tool_name: 'machinelearning',
+    video_link: '', text: '', images: [], icon: 'machinelearning.png'
   },
   {
     id: 51,
-    quiz_name: 'visio',
-    file: 'visio.json',
-    img: 'visio.png'
+    tool_name: 'visio',
+    video_link: '', text: '', images: [], icon: 'visio.png'
   },
   {
     id: 52,
-    quiz_name: 'matlab',
-    file: 'matlab.json',
-    img: 'matlab.png'
+    tool_name: 'matlab',
+    video_link: '', text: '', images: [], icon: 'matlab.png'
   },
   {
     id: 53,
-    quiz_name: 'windowsserver',
-    file: 'windows-server.json',
-    img: 'windowsserver.png'
+    tool_name: 'windowsserver',
+    video_link: '', text: '', images: [], icon: 'windowsserver.png'
   },
   {
     id: 54,
-    quiz_name: 'maven',
-    file: 'maven.json',
-    img: 'maven.png'
+    tool_name: 'maven',
+    video_link: '', text: '', images: [], icon: 'maven.png'
   },
   {
     id: 55,
-    quiz_name: 'wordpress',
-    file: 'wordpress.json',
-    img: 'wordpress.png'
+    tool_name: 'wordpress',
+    video_link: '', text: '', images: [], icon: 'wordpress.png'
   },
   {
     id: 56,
-    quiz_name: 'microsoftaccess',
-    file: 'microsoft-access.md.json',
-    img: 'microsoftaccess.png'
+    tool_name: 'microsoftaccess',
+    video_link: '', text: '', images: [], icon: 'microsoftaccess.png'
   },
   {
     id: 57,
-    quiz_name: 'xml',
-    file: 'xml.json',
-    img: 'xml.jpg'
+    tool_name: 'xml',
+    video_link: '', text: '', images: [], icon: 'xml.jpg'
   },
   {
     id: 58,
-    quiz_name: 'microsoftazure',
-    file: 'microsoft-azure.json',
-    img: 'microsoftazure.png'
+    tool_name: 'microsoftazure',
+    video_link: '', text: '', images: [], icon: 'microsoftazure.png'
   }
 ]
